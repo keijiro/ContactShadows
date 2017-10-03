5fc63112e5ffaec4ce583557886bbb677ca2352e
Keijiro Takahashi <keijiro@gmail.com>
2017-10-03 18:07:54 +0900

    initial commit

---
+++ Assets/CustomShadowTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomShadowTest : MonoBehaviour
+{
+    [SerializeField] Light _light;
+    [SerializeField] Shader _shader;
+
+    Material _material;
+
+    void Update()
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    void OnDestroy()
+    {
+        if (_material != null)
+            if (Application.isPlaying)
+                Destroy(_material);
+            else
+                DestroyImmediate(_material);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {
+        if (_material == null)
+        {
+            _material = new Material(_shader);
+            _material.hideFlags = HideFlags.DontSave;
+        }
+
+        if (_light != null)
+            _material.SetVector("_LightDirection", _light.transform.forward);
+
+        Graphics.Blit(source, dest, _material, 0);
+    }
+}
